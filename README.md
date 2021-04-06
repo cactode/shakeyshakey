4a896378e14303982d0b4d2eef4143e2f9d5f08b
Chetan Sharma <schetan098@gmail.com>
2021-04-05 21:11:44 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# shakeyshakey
-Experimental resonator device from hell
+# Shakey Shakey
+A program that uses an accelerometer and a surface transducer to make an object resonate.
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+int main()
+{
+    ThisThread::sleep_for(1000ms);
+}
