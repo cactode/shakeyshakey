fff68f900631c596e4d9e253a14aa8f21c0e12fc
Chetan Sharma <csharma@anduril.com>
2021-04-07 20:00:53 -0800

    Adding STMCubeMX peripheral code for DMA nonsense.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,37 @@
 int main()
 {   
     while (true) {
-        printf("Output on\n");
-        WaveGen::make_sine(10 * 6.283);
-        ThisThread::sleep_for(1000ms);
-        printf("Output off, activated %d times\n", WaveGen::counter);
-        WaveGen::stop_sine();
-        ThisThread::sleep_for(1000ms);
+        
     }
 }
+
+void DMA_init() {
+    __HAL_RCC_DMA1_CLK_ENABLE();
+    HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+}
+
+// Does not start counter by default
+void init_timer() {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;   // enable TIM2 clock
+    TIM2->CR2    |= TIM_TRGO_UPDATE;      // set TRGO to update mode
+    TIM2->PSC     = 0x0;           // no prescaler
+}
+
+// starts timer
+void start_timer_with_period(uint32_t period) {
+    TIM2->CR1    |= TIM_CR1_UDIS;  // disable updates
+    TIM2->ARR     = period;        // set new period
+    TIM2->CR1    |= TIM_CR1_CEN;   // enable timer
+    TIM2->EGR    |= TIM_EGR_UG;    // force new update
+}
+
+// stops timer, resets it
+void stop_timer() {
+    TIM2->CR1    |= TIM_CR1_UDIS;
+    TIM2->ARR     = 0x0;
+    TIM2->CR1    &= ~0x1;
+    TIM2->EGR    |= TIM_EGR_UG;
+}
+
+
--- wavegen.h
+++ wavegen.h
@@ -5,9 +5,3 @@
 #include "arm_math.h"
 #include <chrono>
 
-// quasi-singleton design pattern since we don't want more than one of these floating around
-namespace WaveGen {
-    extern int counter;
-    void make_sine(float frequency);
-    void stop_sine();
-};
